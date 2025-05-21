adce96680258047c27f52ee03082e0c884f3b6a3
MatteoB23 <anycos2323@gmail.com>
2025-05-21 09:28:45 +0200

    add Movie.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -11,8 +11,9 @@ var app = builder.Build();
 app.MapGet("/", () => "Minimal API nach Arbeitsauftrag 2");
 
 // docker run --name mongodb -d -p 27017:27017 -v data:/data/db -e MONGO_INITDB_ROOT_USERNAME=gbs -e MONGO_INITDB_ROOT_PASSWORD=geheim mongo
-app.MapGet("/check", (Microsoft.Extensions.Options.IOptions<DatabaseSettings> options) => {
-    
+app.MapGet("/check", (Microsoft.Extensions.Options.IOptions<DatabaseSettings> options) =>
+{
+
     try
     {
         var mongoDbConnectionString = options.Value.ConnectionString;
@@ -24,8 +25,45 @@ app.MapGet("/check", (Microsoft.Extensions.Options.IOptions<DatabaseSettings> op
     catch (System.Exception e)
     {
         return "Zugriff auf MongoDB funktioniert nicht: " + e.Message;
-    }         
+    }
 
+});
+
+app.MapPost("/api/movies", (Movie movie) =>
+{
+    throw new NotImplementedException();
+});
+
+app.MapGet("api/movies", () =>
+{
+    return true;
+});
+
+app.MapGet("api/movies/{id}", (string id) =>
+{
+    if (id == "1")
+    {
+        var myMovie = new Movie()
+        {
+            Id = "1",
+            Title = "Asterix und Obelix",
+        };
+        return Results.Ok(myMovie);
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
+
+app.MapPut("/api/movies/{id}", (string id, Movie movie) =>
+{
+    throw new NotImplementedException();
+});
+
+app.MapDelete("api/movies/{id}", (string id) =>
+{
+    throw new NotImplementedException();
 });
 
 app.Run();
